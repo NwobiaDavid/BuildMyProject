e7a03fab7503df66a93ac32d9c8443d329dd92ba
NwobiaDavid <dnwobia@gmail.com>
2023-10-08 18:43:20 +0100

    done with the two.jsx functionalities

--- src/components/Two.jsx
+++ src/components/Two.jsx
@@ -1,7 +1,23 @@
+/* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
 
-
-export const Two = () => {
+export default function Two({Techstack, addLanguage}) {
+  const languages = ['python', 'javascript', 'Go', 'c#', 'PHP'];
   return (
-    <div>Two</div>
+    <div>
+      <h2>choose our main language for the project</h2>
+
+      <ul>
+        {languages.map((lang)=>(
+          <li key={lang} onClick={()=> addLanguage(lang)}> {lang} </li>
+        ))}
+      </ul>
+
+      {Techstack.language && (
+        <Link to="/three">
+          <button>next</button>
+        </Link>
+      )}
+    </div>
   )
 }
