da764201adca361d0cf4d66bdfdba92fa1d92d11
NwobiaDavid <dnwobia@gmail.com>
2023-10-08 18:35:33 +0100

    done with the one.jsx functionalities

--- src/components/One.jsx
+++ src/components/One.jsx
@@ -1,7 +1,25 @@
+/* eslint-disable react/prop-types */
+
+import { Link } from "react-router-dom"
 
 
-export const One = () => {
+export default function One({Techstack, addMedium}) {
+  const types = ['mobile app', 'desktop app', 'web app', 'service']
   return (
-    <div>One</div>
+    <div>
+      <h2>what are you trying to build</h2>
+      <ul>
+        {types.map((item)=>(
+          <li key={item} onClick={()=> addMedium(item)} > {item} </li>
+        ))} 
+      </ul>
+
+      {Techstack.medium && (
+        <Link to="/two">
+          <button>next</button>
+        </Link>
+      )}
+      
+    </div>
   )
 }
